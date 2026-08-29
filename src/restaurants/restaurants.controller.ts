d889b3bf10fd9d75e2c73ec91839e87c8d5582b0
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2026-08-29 08:41:53 +0200

    Removes more unused code

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,8 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
-import { MealsService } from '../meals/meals.service';
-import { CreateRestaurantDto } from './dto/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
 
 @Controller('restaurants')
 export class RestaurantsController {
